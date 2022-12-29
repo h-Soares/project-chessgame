5cde3803738c25931ef4c34ab403784efb43d43a
h-Soares <hiagosoares002@gmail.com>
2022-12-29 13:31:12 -0300

    Classes Piece e Board criadas.

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package boardgame;
+
+/** Classe para uma peça do tabuleiro, que tem uma posição no tabuleiro e está associada a um tabuleiro. */
+
+public class Piece {
+    protected Position position; //inicia como nula, sem posição no tabuleiro.
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() { //protected para ficar restrito apenas a essa camada boardgame.
+        return board;
+    }
+}
