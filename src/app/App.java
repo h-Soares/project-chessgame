ff10266b527136c9e7f522b7bba410cca229a6a7
h-Soares <hiagosoares002@gmail.com>
2022-12-29 13:02:01 -0300

    Classe Position criada.

---
+++ src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) {
+        Position test = new Position(5, 4);
+        System.out.println(test);
+    }
+}
---
+++ src/boardgame/Position.java
@@ -0,0 +1,38 @@
+package boardgame;
+
+/** Classe para uma posição no tabuleiro, que contém linha e coluna. */
+
+public class Position {
+    private int row;
+    private int column;
+   
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override 
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("row: " + row);
+        sb.append(", column: " + column);
+        return sb.toString();
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+}
