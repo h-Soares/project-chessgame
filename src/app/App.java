961037f7db6941a7cd9ce655ef0f0574d1f7ae36
h-Soares <hiagosoares002@gmail.com>
2023-01-01 22:18:42 -0300

    Limpando tela e tratando exceções.

--- src/app/App.java
+++ src/app/App.java
@@ -1,9 +1,11 @@
 package app;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import chess.exceptions.ChessException;
 
 public class App {
     public static void main(String[] args) {
@@ -11,16 +13,31 @@ public class App {
         ChessMatch chessMatch = new ChessMatch();
 
         while(true) { //apenas para teste
-            UserInterface.printChessBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UserInterface.readChessPosition(scan);
-            
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UserInterface.readChessPosition(scan);
+            try {
+                UserInterface.clearScreen();
+                UserInterface.printChessBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UserInterface.readChessPosition(scan);
+                
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UserInterface.readChessPosition(scan);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch(ChessException e) { //TENTAR ARRUMAR ESSA REPETIÇÃO.
+                System.out.println(e.getMessage());
+                System.out.println("PRESS ENTER");
+                scan.nextLine();
+                scan.nextLine();
+            }
+            catch(InputMismatchException e) {
+                System.out.println(e.getMessage());
+                System.out.println("PRESS ENTER");
+                scan.nextLine();
+                scan.nextLine();
+            }
         }
         //scan.close();
     }
